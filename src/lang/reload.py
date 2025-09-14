95740ac77101388817650be576246e3d9c58810c
IsaqueS <isaquesalmeida@outlook.com>
2025-09-14 11:53:15 -0300

    Backup 2

---
+++ src/lang/reload.py
@@ -0,0 +1,39 @@
+import json, csv, os
+from pathlib import Path
+
+LANG_PATH = Path("src/lang/")
+
+if __name__ == "__main__":
+
+    with open(LANG_PATH / "lang.csv", "r", encoding="utf8") as file:
+        reader = csv.reader(file,delimiter=";")
+        # print("FIle")
+
+        languages = next(reader)
+        languages = languages[1:]
+        # print(languages)
+
+        text  = {}
+
+        for language in languages:
+            text[language] = {}
+
+        # print(text)
+
+        for row in reader:
+            # print(row)
+            for id in range(1,len(row)):
+                text[languages[id -1]][row[0]] = row[id]
+        
+        for language_text in text.keys():
+            with open(LANG_PATH / f"{language_text}.json", "w", encoding="utf8") as file:
+                json.dump(text[language_text], file,separators=(',', ':'), ensure_ascii=False)
+
+        def remove_language(path: Path) -> None:
+            if not path.stem in languages:
+                os.remove(path)
+                print("Removed: %s"%path.stem)
+
+        [remove_language(item) for item in LANG_PATH.rglob("*.json")]
+
+        print("Lang file updated!")
